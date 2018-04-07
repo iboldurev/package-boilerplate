e75a53cc04823735f18b4387354cb401ceed84e2
Ivan Boldyrev <iboldurev@gmail.com>
2018-04-07 22:11:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple composer package boilerplate
+
+### License
+
+This project is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Package\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
